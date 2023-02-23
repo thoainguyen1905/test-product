929340b97664bfcef76fa4f47de83227fe36cc7b
Hien Nguyen <hnguyen@madecomfy.com.au>
2023-02-23 14:49:14 +0700

    Test commit with husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
-    const a = 1
     return (
         <div className="App">
             <header className="App-header">
